3289a1ad31be160dfc3bb77039283f1d573a4699
aaronsaikovski <asaikovski@outlook.com>
2023-11-08 16:18:09 +1100

    added list handler code

--- crossword/App/ListBoxHandlers.cs
+++ crossword/App/ListBoxHandlers.cs
@@ -9,32 +9,45 @@ public sealed partial class Crossword
     // Event handler for the Across listbox
     private void SelChangeListClueAcross(object sender, EventArgs args)
     {
-        if (LstClueAcross.SelectedIndex == null) return;
-        SqCurrentSquare.GetClueAnswerRef(IsAcross).HighlightSquares(SqCurrentSquare, false);
-
-        if (IsAcross){
-            IsAcross=true;
-            LstClueDown.SelectedIndex = -1;
-        }
-        SqCurrentSquare = CaPuzzleClueAnswers[(int)LstClueAcross.SelectedIndex].GetSquare();
-        CaPuzzleClueAnswers[(int)LstClueAcross.SelectedIndex].HighlightSquares(SqCurrentSquare, true);
         
-    }
+        if (LstClueAcross.IsKeyboardFocused || LstClueAcross.IsMouseInside)
+        {
+            if (LstClueAcross.SelectedIndex == null) return;
+            SqCurrentSquare.GetClueAnswerRef(IsAcross).HighlightSquares(SqCurrentSquare, false);
+
+            if (IsAcross)
+            {
+                IsAcross = true;
+                LstClueDown.SelectedIndex = -1;
+            }
+
+            SqCurrentSquare = CaPuzzleClueAnswers[(int)LstClueAcross.SelectedIndex].GetSquare();
+            CaPuzzleClueAnswers[(int)LstClueAcross.SelectedIndex].HighlightSquares(SqCurrentSquare, true);
+
+        }
+
+}
 
     //Event handler for the Down listbox
     private void SelChangeListClueDown(object sender, EventArgs args)
     {
-        if (LstClueDown.SelectedIndex == null) return;
-        SqCurrentSquare.GetClueAnswerRef(IsAcross).HighlightSquares(SqCurrentSquare, false);
         
-        if (IsAcross)
+        if (LstClueDown.IsKeyboardFocused || LstClueDown.IsMouseInside)
         {
-            IsAcross = false;
-            LstClueAcross.SelectedIndex = -1;
-        }
+            if (LstClueDown.SelectedIndex == null) return;
+            SqCurrentSquare.GetClueAnswerRef(IsAcross).HighlightSquares(SqCurrentSquare, false);
 
-        SqCurrentSquare = CaPuzzleClueAnswers[LstClueAcross.Items.Count + (int)LstClueDown.SelectedIndex].GetSquare();
-        CaPuzzleClueAnswers[LstClueAcross.Items.Count + (int)LstClueDown.SelectedIndex].HighlightSquares(SqCurrentSquare, true);
+            if (IsAcross)
+            {
+                IsAcross = false;
+                LstClueAcross.SelectedIndex = -1;
+            }
+
+            SqCurrentSquare = CaPuzzleClueAnswers[LstClueAcross.Items.Count + (int)LstClueDown.SelectedIndex]
+                .GetSquare();
+            CaPuzzleClueAnswers[LstClueAcross.Items.Count + (int)LstClueDown.SelectedIndex]
+                .HighlightSquares(SqCurrentSquare, true);
+        }
     }
 
     #endregion
