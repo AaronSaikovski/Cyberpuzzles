e3cb88d0b55ff8bf1ce4f1948dee10b7bbf9f131
aaronsaikovski <asaikovski@outlook.com>
2023-11-20 16:44:44 +1100

    refacxtored

--- crossword/App/KeyDown.cs
+++ crossword/App/KeyDown.cs
@@ -7,8 +7,11 @@ namespace CyberPuzzles.Crossword.App;
 public sealed partial class Crossword
 {
         #region KeyDown
-        
-        //Keypress event - Display the character in the square region
+        /// <summary>
+        /// Keypress event - Display the character in the square region
+        /// </summary>
+        /// <param name="keysDown"></param>
+        /// <param name="keyInFocus"></param>
         public void KeyDown(Keys[] keysDown, Keys keyInFocus)
         {
             if (!PuzzleFinished)
@@ -20,40 +23,10 @@ public sealed partial class Crossword
                 try
                 {
                     //Spacebar pressed to change orientation...bIsAcross.
-                    if (keyInFocus == Keys.Space){
-                        
-                        //Deselect the listbox based on direction
-                        if (!IsAcross)
-                            LstClueDown.SelectedIndex = -1;
-                        else
-                            LstClueAcross.SelectedIndex = -1;
+                    GetSpaceKey(keyInFocus);
 
-                        //Sets the highlighting of the square.
-                        if (SqCurrentSquare != null)
-                        {
-                            SqCurrentSquare.GetClueAnswerRef(IsAcross)?.HighlightSquares(SqCurrentSquare, false);
-
-                            //Change orientation if possible
-                            if (IsAcross)
-                            {
-                                if (SqCurrentSquare.CanFlipDirection(IsAcross))
-                                    IsAcross = false;
-                            }
-                            else
-                            {
-                                if (SqCurrentSquare.CanFlipDirection(IsAcross))
-                                    IsAcross = true;
-                            }
-
-                            //Sets the highlighting of the square.
-                            SqCurrentSquare.GetClueAnswerRef(IsAcross)
-                                ?.HighlightSquares(SqCurrentSquare, true);
-                        }
-                    }
-                    
                     //If the applet has the focus then allow the arrow keys to navigate around
                     //NavigatePuzzle(keyInFocus);
-                    
                     
                     //Only allow list box navigation if they have the focus.
                     //Up and down arrows for the listbox navigation
@@ -68,32 +41,15 @@ public sealed partial class Crossword
                     }
                     
                     //Delete present square's contents if Delete key is pressed
-                    if (keyInFocus == Keys.Delete){
-                        SqCurrentSquare?.SetLetter(' ', IsAcross);
-                    }
-                    
+                    GetDeleteKey(keyInFocus);
+
                     //Check to see if a backspace was entered
-                    if (keyInFocus == Keys.Back){
-                        SqCurrentSquare?.SetLetter(' ', IsAcross);
-                        SqCurrentSquare = SqCurrentSquare?.GetPrevSq(IsAcross);
-                        SqCurrentSquare?.GetClueAnswerRef(IsAcross)?.HighlightSquares(SqCurrentSquare, true);
-                     
-                    }
-                    
+                    GetBackspaceKey(keyInFocus);
+
+
                     //Check that the char falls into our range.
-                    //if ((key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z')) {
-                    if (keyInFocus is >= Keys.A and <= Keys.Z) 
-                    {
-                        //Sets the letter in the current square
-                        SqCurrentSquare?.SetLetter(char.ToUpper((char)keyInFocus), IsAcross);
-
-                        //get next sq or myself(same sq)  if not available
-                        SqCurrentSquare = SqCurrentSquare?.GetNextSq(IsAcross);
-
-                        //Sets the highlighting of the square.
-                        SqCurrentSquare?.GetClueAnswerRef(IsAcross)?.HighlightSquares(SqCurrentSquare, true);
-
-                    }
+                    GetCharKey(keyInFocus);
+                    
                     //update score
                     UpdateCrosswordScore();
                 }
@@ -107,4 +63,100 @@ public sealed partial class Crossword
         }
         #endregion
 
+        #region GetSpaceKey
+        /// <summary>
+        /// Spacebar pressed to change orientation...bIsAcross.
+        /// </summary>
+        /// <param name="keyInFocus"></param>
+        private void GetSpaceKey(Keys keyInFocus)
+        {
+            //Spacebar pressed to change orientation...bIsAcross.
+            if (keyInFocus == Keys.Space)
+            {
+                //Deselect the listbox based on direction
+                if (!IsAcross)
+                    LstClueDown.SelectedIndex = -1;
+                else
+                    LstClueAcross.SelectedIndex = -1;
+
+                //Sets the highlighting of the square.
+                if (SqCurrentSquare != null)
+                {
+                    SqCurrentSquare.GetClueAnswerRef(IsAcross)?.HighlightSquares(SqCurrentSquare, false);
+
+                    //Change orientation if possible
+                    if (IsAcross)
+                    {
+                        if (SqCurrentSquare.CanFlipDirection(IsAcross))
+                            IsAcross = false;
+                    }
+                    else
+                    {
+                        if (SqCurrentSquare.CanFlipDirection(IsAcross))
+                            IsAcross = true;
+                    }
+
+                    //Sets the highlighting of the square.
+                    SqCurrentSquare.GetClueAnswerRef(IsAcross)
+                        ?.HighlightSquares(SqCurrentSquare, true);
+                }
+            }
+        }
+        #endregion
+
+        #region GetDeleteKey
+        /// <summary>
+        /// Delete present square's contents if Delete key is pressed
+        /// </summary>
+        /// <param name="keyInFocus"></param>
+        private void GetDeleteKey(Keys keyInFocus)
+        {
+            //Delete present square's contents if Delete key is pressed
+            if (keyInFocus == Keys.Delete)
+            {
+                SqCurrentSquare?.SetLetter(' ', IsAcross);
+            }
+        }
+        #endregion
+
+        #region GetBackspaceKey
+        /// <summary>
+        /// Gets backspace key press
+        /// </summary>
+        /// <param name="keyInFocus"></param>
+        private void GetBackspaceKey(Keys keyInFocus)
+        {
+            //Check to see if a backspace was entered
+            if (keyInFocus == Keys.Back)
+            {
+                SqCurrentSquare?.SetLetter(' ', IsAcross);
+                SqCurrentSquare = SqCurrentSquare?.GetPrevSq(IsAcross);
+                SqCurrentSquare?.GetClueAnswerRef(IsAcross)?.HighlightSquares(SqCurrentSquare, true);
+            }
+        }
+        #endregion
+
+        #region GetCharKey
+        /// <summary>
+        /// Check that the char falls into our range.
+        /// </summary>
+        /// <param name="keyInFocus"></param>
+        private void GetCharKey(Keys keyInFocus)
+        {
+            //Check that the char falls into our range.
+            //if ((key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z')) {
+            if (keyInFocus is >= Keys.A and <= Keys.Z)
+            {
+                //Sets the letter in the current square
+                SqCurrentSquare?.SetLetter(char.ToUpper((char)keyInFocus), IsAcross);
+
+                //get next sq or myself(same sq)  if not available
+                SqCurrentSquare = SqCurrentSquare?.GetNextSq(IsAcross);
+
+                //Sets the highlighting of the square.
+                SqCurrentSquare?.GetClueAnswerRef(IsAcross)?.HighlightSquares(SqCurrentSquare, true);
+            }
+        }
+        #endregion
+        
 }
