7b85c587894a55aa4cf262ca248f8598c9ee0062
aaronsaikovski <asaikovski@outlook.com>
2024-04-08 14:49:31 +1000

    minor

--- Crossword/Program.cs
+++ Crossword/Program.cs
@@ -4,9 +4,3 @@
 using var game = new CrosswordApp();
 game.Run();
 
-//Implements the builder pattern
-// ICrosswordBuilder builder = new CrosswordBuilder();
-// CrosswordDirector director = new CrosswordDirector(builder);
-// director.Construct();
-// CrosswordPuzzle puzzle = builder.GetResult();
-// puzzle.PrintPuzzle();
