7d51d93bea0b947881627a20a25f949009d015d2
aaronsaikovski <asaikovski@outlook.com>
2024-04-08 14:59:45 +1000

    minor refactor

--- Crossword/UI/DrawSquare.cs
+++ Crossword/UI/DrawSquare.cs
@@ -12,42 +12,6 @@ public sealed partial class CrosswordApp
 {
     #region DrawSquares
 
- 
-    // private bool DrawSquare(int i, int j)
-    // {
-    //     try
-    //     {
-    //         _logger.LogInformation("Start DrawSquares()");
-    //
-    //         //Check to see if a repaint is required
-    //         if (!_sqPuzzleSquares[i, j]!.IsDirty) return true;
-    //         if (_sqPuzzleSquares[i, j]!.BackColour.Equals(UiConstants.SquareHighlightNone))
-    //         {
-    //             if (_puzzleSquares is not null)
-    //                 _spriteBatch.Draw(_imgNormalSquare, _puzzleSquares[i, j], _rectangleColor);
-    //         }
-    //
-    //         if (_sqPuzzleSquares[i, j]!.BackColour.Equals(UiConstants.SquareHighlightWord))
-    //         {
-    //             if (_puzzleSquares is not null)
-    //                 _spriteBatch.Draw(_imgSquareWord, _puzzleSquares[i, j], _rectangleColor);
-    //         }
-    //
-    //         if (!_sqPuzzleSquares[i, j]!.BackColour.Equals(UiConstants.SquareHighlightCurrent)) return false;
-    //         if (_puzzleSquares is not null)
-    //             _spriteBatch.Draw(_imgHighliteSquare, _puzzleSquares[i, j], _rectangleColor);
-    //
-    //         return false;
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         _logger.LogError(ex, ex.Message);
-    //         throw;
-    //     }
-    // }
-
-
-
     /// <summary>
     /// Draws a square
     /// </summary>
